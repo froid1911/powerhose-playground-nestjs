d358efcd3a5025f4a65cc927836ebe843b5ab76d
Frank <frank.pfeift@protonmail.com>
2023-11-06 15:42:24 +0100

    feat: implemented v0 of app service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,24 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  Post,
-  Query,
-  Req,
-  Request,
-} from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AddFileInput } from 'document-model-libs/document-drive';
+import { Document } from 'document-model/document';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/document')
-  getDocument(@Query('drive') drive, @Query('path') path): string {
+  getDocument(@Query('drive') drive, @Query('path') path): Promise<string> {
+    Logger.debug(`getDocument: ${drive} ${path}`);
     return this.appService.getDocument(drive, path);
   }
 
   @Post('/document')
-  addDocument(@Body('path') path, @Body('document') document): boolean {
+  addDocument(
+    @Body('path') path: AddFileInput,
+    @Body('document') document: Document,
+  ): Promise<boolean> {
+    Logger.debug(`addDocument: ${path.path}`);
     return this.appService.addDocument(path, document);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,103 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import {
+  AddFileInput,
+  DocumentDrive,
+  utils as DriveUtils,
+} from 'document-model-libs/document-drive';
+import { Document } from 'document-model/document';
 
 @Injectable()
 export class AppService {
-  getDocument(drive: string, path: string): string {
-    return `${drive} ${path}`;
+  store: any;
+  document: DocumentDrive;
+  intialState = {
+    name: 'My Local Drives',
+    state: {
+      drives: [
+        {
+          id: '123',
+          name: 'Local Device',
+          hash: 'test',
+          nodes: [
+            {
+              name: 'Document Models',
+              path: 'Document Models',
+              hash: 'folder',
+              kind: 'folder',
+            },
+            {
+              name: 'Address Book',
+              path: 'Document Models/addressBook.phdm.zip',
+              hash: 'Address Book',
+              kind: 'file',
+              documentType: 'powerhouse/document-model',
+            },
+            {
+              name: 'Document Drive',
+              path: 'Document Models/documentDrive.phdm.zip',
+              hash: 'Document Drive',
+              kind: 'file',
+              documentType: 'powerhouse/document-model',
+            },
+            {
+              name: 'Document Editor',
+              path: 'Document Models/documentEditor.phdm.zip',
+              hash: 'Document Editor',
+              kind: 'file',
+              documentType: 'powerhouse/document-model',
+            },
+          ],
+        },
+      ],
+    },
+  };
+
+  constructor() {
+    this._init();
   }
 
-  addDocument(path: string, document: any): boolean {
-    console.log(path, document);
+  private async saveFile(path: string, document: Document) {
+    return await this.store.put(path, JSON.stringify(document));
+  }
+
+  async _init() {
+    const name = 'connect-drive-test';
+    const IPFS = await import('ipfs-core');
+    const ORBITDB = await import('@orbitdb/core');
+    const ipfs = await IPFS.create();
+    const orbitdb = await ORBITDB.createOrbitDB({ ipfs });
+    this.store = await orbitdb.open(name, { type: 'keyvalue' });
+    this.document = new DocumentDrive(this.intialState);
+    Logger.log('init');
+  }
+
+  getDrive(id: string) {
+    return this.document.state.drives.find((drive) => drive.id === id);
+  }
+
+  getNode(driveId: string, path: string) {
+    const drive = this.getDrive(driveId);
+    return drive?.nodes.find((n) => n.path === path);
+  }
+
+  async getDocument(driveId: string, path: string): Promise<string> {
+    const file = this.getNode(driveId, path);
+    if (!file) {
+      throw new Error(`Node with path ${path} not found on drive ${driveId}`);
+    }
+    if (!DriveUtils.isFileNode(file)) {
+      throw new Error(
+        `Node with path ${path} on drive ${driveId} is not a file`,
+      );
+    }
+
+    const data = await this.store.get(path);
+    return data;
+  }
+
+  async addDocument(input: AddFileInput, document: Document): Promise<boolean> {
+    await this.saveFile(input.path, document);
+    this.document.addFile(input);
     return true;
   }
 }
